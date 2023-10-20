5638a04a58d85a1a857c722075ebf31d876f93cd
Charlie-83 <11279546+Charlie-83@users.noreply.github.com>
2023-10-20 07:27:40 +0100

    Lexing

--- source/lex.cpp
+++ source/lex.cpp
@@ -1,5 +1,7 @@
-#include <lex.hpp>
+#include "lex.hpp"
+#include <algorithm>
 #include <spdlog/spdlog.h>
+#include <stdexcept>
 #include <string>
 #include <variant>
 #include <vector>
@@ -12,14 +14,29 @@ std::vector<Token> getTokens()
     while (true)
     {
         char character = getchar();
-        if (character == ' ' || character == EOF)
+
+        std::vector<char> terminal_characters = {' ', EOF, '\n', '(', ')', '{', '}', '[', ']'};
+        if (std::find(terminal_characters.begin(), terminal_characters.end(), character) !=
+            terminal_characters.end())
         {
             switch (accumulator_state)
             {
             case AccumulatorState::string:
-                if (accumulator == "function")
+                if (accumulator == "")
+                    break;
+                else if (accumulator == "function")
                 {
                     tokens.emplace_back(TokenType::function, nullptr);
+                    break;
+                }
+                else if (accumulator == "string")
+                {
+                    tokens.emplace_back(TokenType::type, DataType::string);
+                    break;
+                }
+                else if (accumulator == "number")
+                {
+                    tokens.emplace_back(TokenType::type, DataType::number);
                     break;
                 }
                 tokens.emplace_back(TokenType::id, accumulator);
@@ -33,10 +50,33 @@ std::vector<Token> getTokens()
                 tokens.emplace_back(TokenType::other, accumulator);
                 break;
             }
+            switch (character)
+            {
+            case EOF:
+                return tokens;
+            case '\n':
+                tokens.emplace_back(TokenType::newline, nullptr);
+            case '(':
+                tokens.emplace_back(TokenType::open_parenthesis, nullptr);
+            case ')':
+                tokens.emplace_back(TokenType::close_parenthesis, nullptr);
+            case '{':
+                tokens.emplace_back(TokenType::open_brace, nullptr);
+            case '}':
+                tokens.emplace_back(TokenType::close_brace, nullptr);
+            case '[':
+                tokens.emplace_back(TokenType::open_bracket, nullptr);
+            case ']':
+                tokens.emplace_back(TokenType::close_bracket, nullptr);
+            }
 
             if (character == EOF)
             {
                 return tokens;
+            }
+            else if (character == '\n')
+            {
+                tokens.emplace_back(TokenType::newline, nullptr);
             }
 
             accumulator_state = AccumulatorState::none;
@@ -60,5 +100,3 @@ std::vector<Token> getTokens()
         }
     }
 }
-
-Node getAST(std::vector<Token> tokens) {}
--- source/lex.hpp
+++ source/lex.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <fmt/core.h>
 #include <string>
 #include <variant>
@@ -14,14 +15,27 @@ enum class TokenType
     id,
     number,
     function,
-    other,
     eof,
+    type,
+    newline,
+    open_parenthesis,
+    close_parenthesis,
+    open_bracket,
+    close_bracket,
+    open_brace,
+    close_brace,
+};
+
+enum class DataType
+{
+    string,
+    number
 };
 
 struct Token
 {
     TokenType type;
-    std::variant<std::string, double, std::nullptr_t> data;
+    std::variant<std::string, double, DataType, std::nullptr_t> data;
 };
 
 enum class AccumulatorState
@@ -32,58 +46,4 @@ enum class AccumulatorState
     unknown,
 };
 
-struct Node
-{
-};
-
-enum class Type
-{
-    string,
-    number
-};
-
-enum class BinaryOp
-{
-    plus,
-};
-
-struct PrototypeNode : Node
-{
-    std::string name;
-    std::vector<Type> types;
-    std::vector<std::string> args;
-};
-
-struct FunctionNode : Node
-{
-    PrototypeNode prototype;
-};
-
-struct ExpressionNode : Node
-{
-};
-
-struct BinaryOpNode : Node
-{
-    ExpressionNode left;
-    BinaryOp operation;
-    ExpressionNode right;
-};
-
-struct VariableNode : Node
-{
-    std::string name;
-};
-
-struct NumberNode : Node
-{
-    double value;
-};
-
-struct SequenceNode : Node
-{
-    std::vector<Node> nodes;
-};
-
 std::vector<Token> getTokens();
-Node getAST(std::vector<Token> tokens);
